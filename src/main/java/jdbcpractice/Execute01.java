0bd1d59305d8e1a5794d53315caf0da6165a7140
YaseminOrman <jazmintunc@gmail.com>
2022-12-08 23:03:24 -0600

    first commit

---
+++ src/main/java/jdbcpractice/Execute01.java
@@ -0,0 +1,9 @@
+package jdbcpractice;
+import java.sql.*;//1.step
+public class Execute01 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");//2.step
+
+
+    }
+}
